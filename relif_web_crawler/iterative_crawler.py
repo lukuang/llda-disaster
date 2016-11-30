ca0686f7693095148c9474aff872d52ccbd12430
lukuang <lukuang1989@gmail.com>
2016-11-30 14:22:20 -0500

    When crawling failed, print the current time

--- relif_web_crawler/iterative_crawler.py
+++ relif_web_crawler/iterative_crawler.py
@@ -37,6 +37,7 @@ class Iterative_Crawler(object):
             r = requests.get(url_now, params=params)
         except Exception as e:
             print e
+            print time.ctime()
             print "Wait 10 mins and try again"
             command = ["nmcli",'nm','wifi']
             p1 = subprocess.Popen(command,stdout=subprocess.PIPE)
